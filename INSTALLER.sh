d690b0955ac148fa8ad9b36f4b60f4e2368f30c9
Tfloow <88899420+Tfloow@users.noreply.github.com>
2023-01-20 21:22:25 +0100

    upgraded the INSTALLER; need to handle more environnements exception --> Open issues if you encounter something !

--- INSTALLER.sh
+++ INSTALLER.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 pip install pre-commit
-pre-commit install
-echo "Thank you for installing for further instructions or info visit: https://www.github.com/Tfloow/Q4_EPL Thank you :)"
+var=$(pre-commit install)
+echo $var
+
+if [ "$var" = "bash: pre-commit: command not found" ] || [ "$var" = "pre-commit: command not found"]
+then
+  echo "${RED}Oops, something went wrong with the installation of your pre-commit, open an issue on github at${NC}"
+else
+  echo "Thank you for installing for further instructions or info visit: https://www.github.com/Tfloow/Q4_EPL Thank you :)"
+fi
+
+location=$(where pre-commit)
+
+if [ "${location:${#location}-4:4}" = ".exe" ]
+then
+  echo "Well done everything is set!"
+fi
+
 sleep 10
