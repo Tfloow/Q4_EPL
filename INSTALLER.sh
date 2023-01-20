5f27b4b6788076728fd891c4f4db7bebca472cb0
Tfloow <88899420+Tfloow@users.noreply.github.com>
2023-01-20 20:47:01 +0100

    better installer

--- INSTALLER.sh
+++ INSTALLER.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pip install pre-commit
 pre-commit install
-#rm -f INSTALLER.sh
+echo "Thank you for installing for further instructions or info visit: https://www.github.com/Tfloow/Q4_EPL Thank you :)"
+sleep 10
