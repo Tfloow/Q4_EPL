1089296296f22dba1abf286c83f59a97a21c1efd
Tfloow <88899420+Tfloow@users.noreply.github.com>
2023-03-20 19:22:31 +0100

    More precise definition thanks to Samy Mounir

---
+++ SynthèseCompilée - Copie/Sorting.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+def onlyPDF(elemList):
+    """
+    :param elemList: a List of element in a directory
+    :return: only the pdf Document
+    """
+    good = []
+    for elem in elemList:
+        if(elem.split(".")[-1].lower() == "pdf"):
+            good.append(elem)
+    return good
+
+def run():
+    folderList = ["Elec", "FYKI", "GBio", "GC", "Info", "MAP", "Méca", "TroncCommun"] # name of different folder
+    target = "..\\SynthèseCompilée" # were to put the pdf
+    syntheseFolder = "..\\Synthèse" # from where they should be
+
+    for i in os.walk(syntheseFolder): # going through all the folder and files of the Synthèse folder
+        if len(onlyPDF(i[-1])) > 0:
+            for pdf in onlyPDF(i[-1]):
+                shutil.copy(i[0] + "\\" + pdf, target + "\\" + pdf) # copying the pdf to the target folder
+    print("Synthèse(s) copiée(s)")
+
+if __name__ == "__main__":
+    run()
